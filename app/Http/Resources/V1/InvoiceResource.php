7e5f51407daf0db40aa3aecad811b2affc6338f0
Mark 079 <joaopedromaximo@gmail.com>
2023-10-23 09:57:38 -0300

    API return standardization

---
+++ app/Http/Resources/V1/InvoiceResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InvoiceResource extends JsonResource
+{
+    
+    private array $types = ['C' => 'Crédito', 'D' => 'Débito', 'P' => 'Pix'];
+
+    public function toArray(Request $request): array
+    {
+        $paid = $this->paid;
+        return [
+            'user' => [
+                "firstName" => $this->user->firstName,
+                "lastName" => $this->user->lastName,
+                "fullName" => $this->user->firstName . ' ' . $this->user->lastName,
+                "email" => $this->user->email
+            ],
+            'type' => $this->types[$this->type],
+            'paid' => $paid ? 'Pago' : 'Não Pago',
+            'value' =>'R$ ' . number_format($this->value, 2, ',','.'),
+            'paymentDate' => Carbon::parse($this->payment_date)->format('Y/m/d'),
+            'paymentSince' => Carbon::parse($this->payment_date)->diffForHumans()
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\V1\InvoiceController;
+use App\Http\Controllers\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    // users
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+
+    // invoices
+    Route::get('/invoices', [InvoiceController::class, 'index']);
+    Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
 });
